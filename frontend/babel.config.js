c0f42e0410240eff72f8dcbfc54aff2ee7ef879d
Alex Han <alexhan0324@gmail.com>
2023-04-18 20:49:42 -0700

    more tests

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
-    presets: ['@babel/preset-react'],
+    presets: 
+    [
+      [
+        "@babel/preset-env",
+        {
+          "targets": {
+            "node": "current"
+          }
+        }
+      ],
+      "@babel/preset-typescript",
+      '@babel/preset-react'
+    ],
   };
