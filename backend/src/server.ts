f8f6c159f539bbcf7f606cad2a8dcfdb5f63956b
Liang Liu <stuffbyliang@gmail.com>
2023-04-18 20:19:07 -0700

    Add backend persistence

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import { createAutomergeBoard } from "./board";
 import * as Automerge from "@automerge/automerge";
+import fs from "fs";
+import { Board } from "./types";
 
 const express = require('express');
 const app = express();
@@ -16,8 +18,17 @@ const io = new Server(server, {
     }
 });
 
+let board: Board;
 
-let board = createAutomergeBoard(40);
+if (fs.existsSync('board')) {
+    const boardBinary = fs.readFileSync('board');
+    board = Automerge.load(new Uint8Array(boardBinary));
+    console.log("loaded board of size " + boardBinary.length)
+} else {
+    board = createAutomergeBoard(40);
+
+}
+let counter = 0;
 
 io.on('connection', (socket: any) => {
     console.log('a user connected');
@@ -28,9 +39,17 @@ io.on('connection', (socket: any) => {
     });
 
     socket.on('board', ({ boardBinary }: any) => {
+        counter++;
         const otherBoard: any = Automerge.load(new Uint8Array(boardBinary));
         console.log("received board of size     " + boardBinary.length)
         board = Automerge.merge(board, otherBoard);
+
+        // save board every 10 merges
+        if (counter % 10 === 0) {
+            console.log("saving board to disk" + Automerge.save(board).length)
+            fs.writeFileSync('board', Automerge.save(board));
+        }
+
         // broadcast to all clients the merged board
         io.emit('board', { boardBinary: Automerge.save(board) });
     });
